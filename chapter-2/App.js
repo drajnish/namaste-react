b14b55e179a36afae3b87f0891257039cc34a0f9
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2023-01-14 10:07:49 +0530

    chapter-2

---
+++ chapter-2/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// createElement(type, props, ...children)
+const heading = React.createElement('h1', { id: 'title' }, 'Heading 1');
+
+const heading2 = React.createElement('h2', { id: 'title2' }, 'Heading 2');
+
+const content = React.createElement('div', { className: 'content' }, [
+  heading2,
+  'Some random content',
+]);
+
+const container = React.createElement(
+  'div',
+  {
+    id: 'container',
+  },
+  [heading, content]
+);
+
+// console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//passing a react element inside the root
+
+root.render(container);
