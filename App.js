79f42e2aa4ceb4597a32c641e4718b991694bee7
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2023-01-21 07:45:46 +0530

    basic structure and hard coded restaurant card added

--- App.js
+++ App.js
@@ -1,12 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () => {
-  return <h1>Food Villa</h1>;
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items (right side)
+ *  - Cart
+ *
+ * Body
+ *  - Search bar
+ *  - Restaurant List
+ *      - RestaurantCards
+ *          - Image
+ *          - Name
+ *          - Rating
+ *          - Cusines
+ *
+ * Footer
+ *  - Links
+ *  - Copyright
+ *
+ */
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      src="https://yt3.googleusercontent.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+      alt="logo"
+    />
+  </a>
+);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const burgerKing = {
+  name: 'Burger King',
+  image:
+    'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/kack9oun6qtoz1tzbt0i',
+  cusines: ['Burger', 'American'],
+  rating: '4.2',
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="card">
+      <img src={burgerKing.image} alt="Burger king" />
+      <h2>{burgerKing.name}</h2>
+      <h3>{burgerKing.cusines.join(', ')}</h3>
+      <h4>{burgerKing.rating} stars</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<AppLayout />);
 
-export default App;
+export default AppLayout;
